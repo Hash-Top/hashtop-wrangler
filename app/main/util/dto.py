3c306a0f071e1cc039138e4ff5e18e305a2b123b
Evan Huang <evanhuang117@gmail.com>
2021-04-04 18:54:07 -0400

    fix userstats dto model

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -13,12 +13,12 @@ class UserDto:
 
     stat = api.model('userStat', {
         'time': fields.DateTime(required=True, description="time that the stats were queried"),
-        'wallet_addr': fields.DateTime(required=True, description="which wallet address the stats are for"),
+        'wallet_addr': fields.String(required=True, description="which wallet address the stats are for"),
         'balance': fields.Float(required=False, description="total balance in wei"),
         'est_revenue': fields.Float(required=False, description="flexpools estimated daily revenue in wei"),
-        'valid_shares': fields.Float(required=False, description="total valid shares over past 24hrs"),
-        'stale_shares': fields.Float(required=False, description="total stale shares over past 24hrs"),
-        'invalid_shares': fields.Float(required=False, description="total invalid shares over past 24hrs"),
+        'valid_shares': fields.Integer(required=False, description="total valid shares over past 24hrs"),
+        'stale_shares': fields.Integer(required=False, description="total stale shares over past 24hrs"),
+        'invalid_shares': fields.Integer(required=False, description="total invalid shares over past 24hrs"),
         'round_share_percent': fields.Float(required=False, description="share percentage of the current round"),
         'effective_hashrate': fields.Float(required=False, description="current effective hashrate reported by flexpool"),
     })
