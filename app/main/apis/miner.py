e92347ead3693af539c1e142f20697c8630d2080
Evan Huang <evanhuang117@gmail.com>
2021-05-27 12:05:37 -0700

    modified dto to better support api calls from dashboard

--- app/main/apis/miner.py
+++ app/main/apis/miner.py
@@ -11,7 +11,8 @@ from ..service.miner_service import (
     get_shares_by_miner,
 )
 from ..service.user_service import (
-    get_user
+    get_user_by_username,
+    get_user_by_id
 )
 from ..service import update, delete
 from ..util.decorators import token_required
@@ -24,17 +25,32 @@ _share = MinerDto.share
 stats_schema = MinerDto.StatsQuerySchema()
 
 
+@api.route('/<user_id>/')
+@api.param('user_id', "User's id")
+@api.response(404, 'No User with that id.')
+class MinerList(Resource):
+    @api.doc('list_of_miners_owned_by_this_user')
+    @api.marshal_list_with(_miner, envelope='data')
+    # TODO: remove before prod
+    # @token_required
+    def get(self, user_id):
+        """List all miners owned by this user"""
+        user = get_user_by_id(user_id=user_id)
+        return get_all_miners(user)
+
+
 @api.route('/<username>/<miner_id>')
 @api.param('username', "User's username")
 @api.param('miner_id', "The miner's id")
 @api.response(404, 'No User/Miner with that id.')
-class MinerList(Resource):
+class Miner(Resource):
     @api.doc('list_of_miners_owned_by_this_user')
     @api.marshal_list_with(_miner, envelope='data')
-    @token_required
+    # TODO: remove before prod
+    # @token_required
     def get(self, username):
         """List all miners owned by this user"""
-        user = get_user(username=username)
+        user = get_user_by_username(username=username)
         return get_all_miners(user)
 
     @api.response(201, 'Miner successfully created.')
@@ -45,7 +61,7 @@ class MinerList(Resource):
     def post(self, username):
         """Creates a new Miner """
         data = request.json
-        user = get_user(username=username)
+        user = get_user_by_username(username=username)
         return create_new_miner(data, user)
 
     @api.response(200, 'Miner successfully updated.')
@@ -54,7 +70,7 @@ class MinerList(Resource):
     @token_required
     def put(self, username, miner_id):
         """Updates a User's miner """
-        user = get_user(username)
+        user = get_user_by_username(username)
         miner = get_miner(miner_id)
         if not user or not miner:
             api.abort(404)
@@ -74,8 +90,7 @@ class MinerList(Resource):
             return delete(miner)
 
 
-@api.route('/<username>/<miner_id>/health')
-@api.param('username', "User's username")
+@api.route('<miner_id>/health')
 @api.param('miner_id', "The miner's id")
 @api.response(404, 'No User/Miner with that id.')
 class MinerHealths(Resource):
@@ -85,11 +100,10 @@ class MinerHealths(Resource):
     @api.marshal_list_with(_health)
     # TODO: resecure before full prod deploy
     # @token_required
-    def get(self, username, miner_id):
+    def get(self, miner_id):
         """ Get a miner's temp, hashrate and power usage by gpu. Not specifying start or end will return the past 7 days of stats """
-        user = get_user(username)
         miner = get_miner(miner_id)
-        if not user:
+        if not miner:
             api.abort(404)
         else:
             """validate the query params"""
@@ -100,10 +114,9 @@ class MinerHealths(Resource):
         return get_healths_by_miner(miner, request.args.get('start'), request.args.get('end'))
 
 
-@api.route('/<username>/<miner_id>/share')
-@api.param('username', "User's username")
+@api.route('/<miner_id>/share')
 @api.param('miner_id', "The miner's id")
-@api.response(404, 'No User/Miner with that id.')
+@api.response(404, 'No Miner with that id.')
 class MinerShares(Resource):
     # TODO: add default limits ^
     @api.param('end', "Optional time after which to retrieve stats")
@@ -111,11 +124,10 @@ class MinerShares(Resource):
     @api.marshal_list_with(_share)
     # TODO: resecure before full prod deploy
     # @token_required
-    def get(self, username, miner_id):
+    def get(self, miner_id):
         """ Get a miners shares by gpu. Not specifying start or end will return the past 7 days of stats """
-        user = get_user(username)
         miner = get_miner(miner_id)
-        if not user:
+        if not miner:
             api.abort(404)
         else:
             """validate the query params"""
